96dff457d3f9dd0c0126d96d00137ef70de5e903
sanghee01 <sang6299@naver.com>
2022-07-17 22:04:12 +0900

    초안 다시 작성 - 대사부분 시작

--- js/player.js
+++ js/player.js
@@ -1,11 +1,22 @@
 const player = document.querySelector("#player");
-let x = 0;
-let y = 0;
 
-const npc = document.querySelectorAll(".npc");
+const npc1 = document.querySelector("#npc1");
+const npc2 = document.querySelector("#npc2");
+const npc3 = document.querySelector("#npc3");
 
-npc[0].addEventListener("click", (e) => {
-  x = e.clientX;
-  y = e.clientY;
-  player.style.transform = "translate(" + (x - 600) + "px," + (y - 205) + "px)";
-});
+const talk = document.querySelector(".talk");
+const overlay = document.querySelector(".talk__overlay");
+const closeBtn = document.querySelector(".close");
+
+const openTalk = () => {
+  talk.classList.remove("hidden");
+};
+const closeTalk = () => {
+  talk.classList.add("hidden");
+};
+
+overlay.addEventListener("click", closeTalk);
+closeBtn.addEventListener("click", closeTalk);
+npc1.addEventListener("click", openTalk);
+npc2.addEventListener("click", openTalk);
+npc3.addEventListener("click", openTalk);
